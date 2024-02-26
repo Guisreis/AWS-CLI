a14c36edff0c0b53eeb49a01a8ecd874b22734c0
Guilherme dos Santos Reis <86111129+Guisreis@users.noreply.github.com>
2024-02-26 17:40:13 -0300

    Update modify_gp2togp3.sh

--- modify-gp2-to-gp3/modify_gp2togp3.sh
+++ modify-gp2-to-gp3/modify_gp2togp3.sh
@@ -2,10 +2,10 @@
 
 region='us-east-1'
 
-# Find all gp2 volumes within the given region
+# Encontre todos os volumes gp2 dentro de uma determinada região
 volume_ids=$(/usr/bin/aws ec2 describe-volumes --region "${region}" --filters Name=volume-type,Values=gp2 | jq -r '.Volumes[].VolumeId')
 
-# Iterate all gp2 volumes and change its type to gp3
+# Agrupe todos os volumes gp2 e altere seu tipo para gp3
 for volume_id in ${volume_ids};do
     result=$(/usr/bin/aws ec2 modify-volume --region "${region}" --volume-type=gp3 --volume-id "${volume_id}" | jq '.VolumeModification.ModificationState' | sed 's/"//g')
     if [ $? -eq 0 ] && [ "${result}" == "modifying" ];then
