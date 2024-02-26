62fb5661fc29677bdd488705c8a2edbbe4224a29
Guilherme dos Santos Reis <86111129+Guisreis@users.noreply.github.com>
2024-02-26 17:38:40 -0300

    Update modify_gp2togp3_porID.sh

--- modify-gp2-to-gp3/modify_gp2togp3_porID.sh
+++ modify-gp2-to-gp3/modify_gp2togp3_porID.sh
@@ -5,7 +5,7 @@ region='us-east-1'
 # Insira manualmente os IDs dos volumes separados por espaço
 volume_ids_array=( "vol-0a0229787f661ee69" "vol-026cbe1d8a78875e4" "vol-06bc533adfec8268e" "vol-08e808e7ba4ed3c6d" "vol-014d5e214817d2a56" )
 
-# Iterar sobre os IDs dos volumes fornecidos
+# AIterar sobre os IDs dos volumes fornecidos
 for volume_id in "${volume_ids_array[@]}"; do
     # Alterar o tipo do volume para gp3
     result=$(/usr/bin/aws ec2 modify-volume --region "${region}" --volume-type=gp3 --volume-id "${volume_id}" | jq '.VolumeModification.ModificationState' | sed 's/"//g')
